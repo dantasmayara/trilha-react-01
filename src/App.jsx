10a4e9f934e20fc517a01e333496c690f9955b9c
dantasmayara <contatomayaradantas@gmail.com>
2022-07-20 11:20:01 -0300

    teste

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author="Diego Fernandes" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, id minus? Expedita quidem ipsam voluptatibus voluptates molestiae, sed voluptatum eum. Itaque perspiciatis possimus voluptatibus, obcaecati modi debitis quia libero inventore."
+      />
+      <Post
+        author="Mayara Dantas"
+        content="Estou emocionada!"
+      />
+        </div>
+  )
+};
